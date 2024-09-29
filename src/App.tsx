bf2b038db98856b23a8e7099a16a4151a6bd4791
KriteshTimsina <kriteshtimsina55@gmail.com>
2024-09-29 11:51:29 +0545

    init

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {Animated, TouchableOpacity, View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+function SettingsScreen() {
+  return (
+    <View>
+      <Text>Hello Settings</Text>
+    </View>
+  );
+}
+
+function MyTabBar({state, descriptors, navigation, position}) {
+  return (
+    <View style={{flexDirection: 'row'}}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        const inputRange = state.routes.map((_, i) => i);
+        const opacity = position.interpolate({
+          inputRange,
+          outputRange: inputRange.map(i => (i === index ? 1 : 0)),
+        });
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{flex: 1}}>
+            <Animated.Text style={{opacity}}>{label}</Animated.Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
