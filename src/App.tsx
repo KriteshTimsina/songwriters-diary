bfcbde7fc4765e6bdc6758b19a8a3ce75db1fe5a
KriteshTimsina <kriteshtimsina55@gmail.com>
2024-09-30 07:58:47 +0545

    feat: handle storage and record permission

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {
+  Alert,
   Animated,
+  PermissionsAndroid,
+  Platform,
   Pressable,
   StyleSheet,
   TouchableOpacity,
+  useColorScheme,
   View,
 } from 'react-native';
 import {Feather} from './components/icons';
@@ -16,8 +20,37 @@ import {TabbarTop} from './navigation';
 import HomeStack from './navigation/HomeStack';
 import TabNavigation from './navigation/TabNavigation';
 import SongProvider from './context/SongContext';
+import {
+  check,
+  PERMISSIONS,
+  request,
+  requestMultiple,
+  RESULTS,
+} from 'react-native-permissions';
+import {useEffect} from 'react';
 
 export default function App() {
+  useEffect(() => {
+    requestAllPermissions();
+  }, []);
+
+  const requestAllPermissions = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const permissionResults = await requestMultiple([
+          PERMISSIONS.ANDROID.RECORD_AUDIO,
+          PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
+          PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+        ]);
+        console.log(permissionResults, 'Permissions');
+      } catch (error) {
+        console.error('Permission request error:', error);
+      }
+    } else {
+      Alert.alert('Permissions are for Android only.');
+    }
+  };
+
   return (
     <NavigationContainer>
       <SongProvider>
